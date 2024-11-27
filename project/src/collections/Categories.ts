fae74017b4ed4b40c91de74b7d2e96f510d44c73
abdul977 <47270099+abdul977@users.noreply.github.com>
2024-11-27 20:13:57 +0100

    Add project/src/collections/Categories.ts

---
+++ project/src/collections/Categories.ts
@@ -0,0 +1,62 @@
+import type { CollectionConfig } from 'payload'
+import { anyone } from '../access/anyone'
+import { authenticated } from '../access/authenticated'
+
+export const Categories: CollectionConfig = {
+  slug: 'categories',
+  access: {
+    create: authenticated,
+    delete: authenticated,
+    read: anyone,
+    update: authenticated,
+  },
+  admin: {
+    useAsTitle: 'title',
+    defaultColumns: ['title', 'parent', 'createdAt'],
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'parent',
+      type: 'relationship',
+      relationTo: 'categories',
+    },
+    {
+      name: 'featured',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+    {
+      name: 'meta',
+      type: 'group',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+        },
+        {
+          name: 'description',
+          type: 'textarea',
+        },
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+        },
+      ],
+    },
+  ],
+}
