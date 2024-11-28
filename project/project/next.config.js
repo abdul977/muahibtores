3270fd1a810fc72176f2fc7512bd9a8218972b5f
abdul977 <47270099+abdul977@users.noreply.github.com>
2024-11-28 09:25:00 +0100

    Add project/project/next.config.js

---
+++ project/project/next.config.js
@@ -0,0 +1,35 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
+import redirects from './redirects.js'
+
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+        const url = new URL(item)
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(':', ''),
+        }
+      }),
+    ],
+  },
+  reactStrictMode: true,
+  redirects,
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['sharp'],
+  }
+}
+
+export default withPayload(nextConfig)
